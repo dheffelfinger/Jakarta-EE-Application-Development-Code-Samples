be60a45dd76acdf2e48b40aa6a21291911ee5de1
David R. Heffelfinger <dheffelfinger@ensode.com>
2023-06-17 10:58:04 -0400

    Updated SSE example.

--- ch03_src/serversentevents/src/main/java/net/ensode/jakartaeebook/serversentevents/SseResource.java
+++ ch03_src/serversentevents/src/main/java/net/ensode/jakartaeebook/serversentevents/SseResource.java
@@ -1,9 +1,8 @@
 package net.ensode.jakartaeebook.serversentevents;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.context.RequestScoped;
-import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -26,14 +25,12 @@ public class SseResource {
 
   private static final Logger LOG = Logger.getLogger(SseResource.class.getName());
 
-  private DoubleStream stockTickerValues = DoubleStream.iterate(10.0, val -> val + 0.9);
+  private final DoubleStream stockTickerValues = DoubleStream.iterate(10.0, val -> val + 0.9);
 
   private SseBroadcaster sseBroadcaster;
   private OutboundSseEvent.Builder eventBuilder;
 
-  private Executor executor = Executors.newSingleThreadExecutor();
-
-  private OutboundSseEvent.Builder outboundSseEventBuilder;
+  private final Executor executor = Executors.newSingleThreadExecutor();
 
   @PostConstruct
   public void init() {
@@ -59,21 +56,17 @@ public class SseResource {
       stockTickerValues.forEach(value -> {
         try {
           TimeUnit.SECONDS.sleep(5);
-          LOG.info(String.format("Sending the following value: %.2f", value));
           final OutboundSseEvent outboundSseEvent = eventBuilder
                   .name("ENSD stock ticker value")
                   .data(String.class, String.format("%.2f", value))
                   .build();
           LOG.info(String.format("broadcasting event: %.2f", value));
-          //broadcaster.broadcast(builder.data(Message.class, message).mediaType(MediaType.APPLICATION_JSON_TYPE).build());
           sseBroadcaster.broadcast(outboundSseEvent);
 
         } catch (InterruptedException ex) {
           LOG.log(Level.SEVERE, "Exception caught", ex);
         }
-
       });
-
     });
   }
 }
