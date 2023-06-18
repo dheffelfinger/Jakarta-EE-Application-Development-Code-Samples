cb15158eda32bf5f7d437ad6652beb98a31928d7
David R. Heffelfinger <dheffelfinger@ensode.com>
2023-06-18 08:24:32 -0400

    Updated SSE example.

--- ch03_src/serversentevents/src/main/java/net/ensode/jakartaeebook/serversentevents/SseResource.java
+++ ch03_src/serversentevents/src/main/java/net/ensode/jakartaeebook/serversentevents/SseResource.java
@@ -12,12 +12,10 @@ import jakarta.ws.rs.sse.OutboundSseEvent;
 import jakarta.ws.rs.sse.Sse;
 import jakarta.ws.rs.sse.SseBroadcaster;
 import jakarta.ws.rs.sse.SseEventSink;
-import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.DoubleStream;
 
 @ApplicationScoped
 @Path("serversentevents")
@@ -25,16 +23,23 @@ public class SseResource {
 
   private static final Logger LOG = Logger.getLogger(SseResource.class.getName());
 
-  private final DoubleStream stockTickerValues = DoubleStream.iterate(10.0, val -> val + 0.9);
-
   private SseBroadcaster sseBroadcaster;
   private OutboundSseEvent.Builder eventBuilder;
 
-  private final Executor executor = Executors.newSingleThreadExecutor();
+  private ScheduledExecutorService scheduledExecutorService;
+
+  private Double stockValue = 10.0;
 
   @PostConstruct
   public void init() {
+    scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+
     sendEvents();
+  }
+
+  @PreDestroy
+  public void cleanup() {
+    scheduledExecutorService.shutdown();
   }
 
   @Context
@@ -52,21 +57,14 @@ public class SseResource {
   }
 
   public void sendEvents() {
-    executor.execute(() -> {
-      stockTickerValues.forEach(value -> {
-        try {
-          TimeUnit.SECONDS.sleep(5);
-          final OutboundSseEvent outboundSseEvent = eventBuilder
-                  .name("ENSD stock ticker value")
-                  .data(String.class, String.format("%.2f", value))
-                  .build();
-          LOG.info(String.format("broadcasting event: %.2f", value));
-          sseBroadcaster.broadcast(outboundSseEvent);
-
-        } catch (InterruptedException ex) {
-          LOG.log(Level.SEVERE, "Exception caught", ex);
-        }
-      });
-    });
+    scheduledExecutorService.scheduleAtFixedRate(() -> {
+      final OutboundSseEvent outboundSseEvent = eventBuilder
+              .name("ENSD stock ticker value")
+              .data(String.class, String.format("%.2f", stockValue))
+              .build();
+      LOG.info(String.format("broadcasting event: %.2f", stockValue));
+      sseBroadcaster.broadcast(outboundSseEvent);
+      stockValue += 0.9;
+    }, 5, 5, TimeUnit.SECONDS);
   }
 }
